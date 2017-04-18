fe24a95eef21935df72052e75616a4ac8d4fea37
rainleon <liangjian0511@yeah.net>
2017-04-18 18:39:19 +0800

    add mongo docker

--- ci.sh
+++ ci.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+
+export GITHUB_SS_PARENT_PREFIX="https://github.com/seek-world/ss-parent/raw/master"
 export GITHUB_GIT_SERVICE_TOKEN="6648f1fe47320706cf6694dbc23b735212b27b71"
 export BUILD_SITE_GITHUB_REPOSITORY_OWNER="rainleon"
 export BUILD_SITE_GITHUB_REPOSITORY_NAME="xss-media"
@@ -11,8 +13,8 @@ export MAVEN_CENTRAL_PASS=""
 export MAVEN_OPTS=""
 
 export MAVEN_OPTS="${MAVEN_OPTS} -Dgithub-nexus.repositories=https://oss.sonatype.org/content/repositories"
-export MAVEN_OPTS="${MAVEN_OPTS} -Dcheckstyle.config.location=${BUILD_SCRIPT_LOC}/src/main/checkstyle/google_checks_6.19.xml"
-export MAVEN_OPTS="${MAVEN_OPTS} -Dpmd.ruleset.location=${BUILD_SCRIPT_LOC}/src/main/pmd/pmd-ruleset-5.3.5.xml"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dcheckstyle.config.location=${GITHUB_SS_PARENT_PREFIX}/src/main/checkstyle/google_checks_6.19.xml"
+export MAVEN_OPTS="${MAVEN_OPTS} -Dpmd.ruleset.location=${GITHUB_SS_PARENT_PREFIX}/src/main/pmd/pmd-ruleset-5.3.5.xml"
 
 export MAVEN_OPTS="${MAVEN_OPTS} -Dbuild.publish.channel=snapshot"
 export MAVEN_OPTS="${MAVEN_OPTS} -Ddocker.registry="
@@ -22,12 +24,26 @@ export MAVEN_OPTS="${MAVEN_OPTS} -Dmaven.integration-test.skip=true"
 export MAVEN_OPTS="${MAVEN_OPTS} -Djacoco=true"
 
 export MAVEN_SETTINGS=""
-export MAVEN_SETTINGS="${MAVEN_SETTINGS} -s src/main/maven/settings.xml"
+
+COMMIT_ID="$(git rev-parse HEAD)"
+CI_CACHE="${HOME}/.ci-cache/${COMMIT_ID}"
+mkdir -p ${CI_CACHE}
+
+if [ ! -f "$(pwd)/src/main/maven/settings.xml" ]; then
+    MAVEN_SETTINGS_FILE="${CI_CACHE}/settings-${COMMIT_ID}.xml"
+    echo "curl -H 'Cache-Control: no-cache' -t utf-8 -s -L -o ${MAVEN_SETTINGS_FILE} ${GITHUB_SS_PARENT_PREFIX}/src/main/maven/settings.xml"
+    curl -H 'Cache-Control: no-cache' -t utf-8 -s -L -o ${MAVEN_SETTINGS_FILE} ${GITHUB_SS_PARENT_PREFIX}/src/main/maven/settings.xml
+    export MAVEN_SETTINGS="${MAVEN_SETTINGS} -s ${MAVEN_SETTINGS_FILE}"
+else
+    export MAVEN_SETTINGS="${MAVEN_SETTINGS} -s $(pwd)/src/main/maven/settings-${INFRASTRUCTURE}.xml"
+fi
 
 echo "MAVEN_OPTS: ${MAVEN_OPTS}"
 
 if [ "${1}" == "clean" ]; then
     mvn ${MAVEN_SETTINGS} clean
+elif [ "${1}" == "help" ]; then
+    mvn ${MAVEN_SETTINGS} help:effective-pom
 else
     mvn ${MAVEN_SETTINGS} clean install
 fi
